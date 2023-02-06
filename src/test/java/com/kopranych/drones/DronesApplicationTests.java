ec8b2a96ab6224dfdbc13e295d7a296fdb9b074d
Kopranych <kopranych@gmail.com>
2023-02-06 10:27:37 +0600

    drones - init project

---
+++ src/test/java/com/kopranych/drones/DronesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kopranych.drones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
