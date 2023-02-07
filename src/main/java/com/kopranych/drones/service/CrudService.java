f431773c98016f4f7120e1730449caf47cf6b4f8
Kopranych <kopranych@gmail.com>
2023-02-07 22:19:41 +0600

    drones - implements DispatchService

---
+++ src/main/java/com/kopranych/drones/service/CrudService.java
@@ -0,0 +1,2 @@
+package com.kopranych.drones.service;public interface CrudService {
+}
---
+++ src/main/java/com/kopranych/drones/service/impl/DispatchService.java
@@ -0,0 +1,9 @@
+package com.kopranych.drones.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface DispatchService<T> {
+
+  
+}
